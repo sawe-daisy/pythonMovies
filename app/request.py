daaaf0b138296d7c2ab9b1360861282bfed37775
sawe-daisy <deesawe16@gmail.com>
2020-10-13 16:54:11 +0300

    error template

--- app/request.py
+++ app/request.py
@@ -10,6 +10,24 @@ api_key = app.config['MOVIE_API_KEY']
 # getting the movie base url
 base_url = app.config['MOVIE_API_BASE_URL']
 
+def get_movie(id):
+    get_movie_details_url = base_url.format(id, api_key)
+    with urllib.request.urlopen(get_movie_details_url) as url:
+        movie_details_data = url.read()
+        movie_details_response = json.loads(movie_details_data)
+        
+        movie_object = None
+        if movie_details_response:
+            id= movie_details_response.get('id')
+            title = movie_details_response.get('original_title')
+            overview = movie_details_response.get('overview')
+            poster = movie_details_response.get('poster_path')
+            vote_average = movie_details_response.get('vote_average')
+            vote_count = movie_details_response.get('vote_count')
+            movie_object = Movie(id, title, poster, vote_average,vote_count)
+
+        return movie_object
+
 
 def get_movies(category):
     get_movies_url = base_url.format(category,api_key)
